cf167bb35a3229984d97dbfdcc1b294e5d3ab7e5
Brian Tu <feihcsim824@gmail.com>
2017-12-04 23:57:16 -0500

    fixed bug in xml parsing

--- news-aggregator/main.go
+++ news-aggregator/main.go
@@ -36,13 +36,18 @@ import (
 
 // can just be reduced to this struct:
 type SitemapIndex struct {
-	Locations []string `xml:"sitemap > loc"`
+	Locations []string `xml:"sitemap>loc"`
 }
 
 type News struct {
-	Titles []string `xml:"url > news > title"`
-	Keywords []string `xml:"url > news > keywords"`
-	Locations []string `xml:"url > loc"`
+	Titles []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+type NewsMap struct {
+	Keyword string
+	Location string
 }
 
 func main() {
@@ -50,10 +55,14 @@ func main() {
 	var s SitemapIndex
 	var n News
 
+	newsMap := make(map[string]NewsMap) // make a map where the keys are strings and the values are NewsMaps
+
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
-
+	// fmt.Printf("%s", resp.Body)
+	
+	xml.Unmarshal(bytes, &s) // unmarshal the data into the news address
 	// fmt.Println(s.Locations) // when printing, go will need a "to string" method, do let's add it to our Location struct
 	
 	// fmt.Printf("Here %s some %s", "are", "variables") // string formatting with placeholders
@@ -62,6 +71,15 @@ func main() {
 		resp, _ := http.Get(Location)
 		bytes, _ := ioutil.ReadAll(resp.Body)
 		xml.Unmarshal(bytes, &n) // unmarshal the data into the news address
+		for idx, _ := range n.Titles {
+			newsMap[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
+	}
+
+	for idx, data := range newsMap {
+		fmt.Println("\n\n\n", idx)
+		fmt.Println("\n", data.Keyword)
+		fmt.Println("\n", data.Location)
 	}
 
 }
